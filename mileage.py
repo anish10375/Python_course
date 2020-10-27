626b050207cc3f7e7584c02a5e8c03c7b10364e9
anish10375 <73473123+anish10375@users.noreply.github.com>
2020-10-27 22:26:22 +0530

    Update mileage.py
    added strings so that the user understands what to input

--- mileage.py
+++ mileage.py
@@ -1,5 +1,7 @@
-odometer_initial = int(input("Enter initial odometer reading "))
-odometer_final   = int(input("Enter final odometer reading "))
+odometer_initial = int(input("Enter initial odometer reading ")) ''' assume odometer_initial is the odometer reading
+...just after refueling upto the full tank capacity'''
+odometer_final   = int(input("Enter final odometer reading ")) ''' assume odometer_final is the odometer reading
+... when the car completely runs out of fuel'''
 fuel_initial     = int(input("Enter initial fuel reading "))
 fuel_final       = int(input("Enter final fuel reading "))
 Mileage          = (odometer_final-odometer_initial)/(fuel_initial-fuel_final)
