2caea8688b13e72408a2ddb42ed66fbd216caeb4
anish10375 <73473123+anish10375@users.noreply.github.com>
2020-10-27 22:44:38 +0530

    Update multiplication _table(17).py
    completely changed program as required

--- multiplication _table(17).py
+++ multiplication _table(17).py
@@ -1,5 +1,13 @@
 
-
-multiplier = int(input("Enter multiplier here "))
-print(multiplier*17)                 
+           multiplicand = int(input(" enter multiplier here "))
+print(" 17 * 1 = " , multiplicand*1)
+print(" 17 * 2 = " , multiplicand*2)
+print(" 17 * 3 = " , multiplicand*3)
+print(" 17 * 4 = " , multiplicand*4)
+print(" 17 * 5 = " , multiplicand*5)
+print(" 17 * 6 = " , multiplicand*6)
+print(" 17 * 7 = " , multiplicand*7)
+print(" 17 * 8 = " , multiplicand*8)
+print(" 17 * 9 = " , multiplicand*9)
+print(" 17 * 10 = " , multiplicand*10)
 
