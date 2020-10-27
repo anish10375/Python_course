efdc295ccd12ec5b16ded62b5104b54eb7352aa0
anish10375 <73473123+anish10375@users.noreply.github.com>
2020-10-27 22:20:03 +0530

    Update no._of_stops.py
    Deleted an unnecessary comment and changed variable name

--- no._of_stops.py
+++ no._of_stops.py
@@ -1,6 +1,4 @@
-
-#fuel_initial is the full tank capacity
-fuel_initial     = int(input("Enter initial fuel reading "))
+full_tank_capacity     = int(input("Enter full tank capacity "))
 Mileage          = float(input("Enter Mileage"))
 #Distance travelled between fuel stops would be the same as we assumed mileage is constant
 no_of_stops      = 560//(fuel_initial*Mileage)
